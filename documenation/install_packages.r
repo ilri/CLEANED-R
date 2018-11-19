c1a3e38e34c7fbbb3ec51d5a3c187f7824fbd0de
Catherine <pfeifer.catherine@gmail.com>
2018-11-19 17:54:17 +0300

    Add files via upload

---
+++ documenation/install_packages.r
@@ -0,0 +1,17 @@
+# this code installes the R packages that are required to run CLEANED
+
+# this code needs to be run after the first installation of R/R studio, and before running CLEANED
+# this only needs to be done once 
+
+#you need to be connected to internet to do this. 
+
+install.packages("sp")
+install.packages("raster")
+install.packages("maptools")
+install.packages("RColorBrewer")
+install.packages("rgdal")
+install.packages("igraph")
+install.packages('gridExtra')
+install.packages('shiny')
+install.packages('shinydashboard')
+install.packages("rgeos")
